a1240dcfd84d7a5568000799350911cf596242c4
4nndr01d <Super0rganism@yandex.ru>
2023-03-09 00:13:16 +0300

    update functional_tests with unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        options = webdriver.FirefoxOptions()
+        options.binary_location = "/usr/bin/firefox"
+        self.browser = webdriver.Firefox(options=options)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
