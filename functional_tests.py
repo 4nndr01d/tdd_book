dd289e340f23958151d41f59cf761acd7c111e8b
4nndr01d <Super0rganism@yandex.ru>
2023-03-10 00:18:26 +0300

    add handle post form and simple tests

--- functional_tests.py
+++ functional_tests.py
@@ -29,12 +29,22 @@ class NewVisitorTest(unittest.TestCase):
         )
         inputbox.send_keys('Купить павлиньи перья')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        self.assertIn(
+            '2: Сделать мушку из павлиньих перьев',
+            [row.text for row in rows]
         )
 
         self.fail('Закончить тест!')
