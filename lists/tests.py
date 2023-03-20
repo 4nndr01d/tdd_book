8b98eebde762472b41e829bab235d9a45e86fea2
4nndr01d <Super0rganism@yandex.ru>
2023-03-21 00:35:35 +0300

    add endpoint for new list

--- lists/tests.py
+++ lists/tests.py
@@ -13,10 +13,6 @@ class HomePageTest(TestCase):
         self.assertTrue(html.endswith('</html>'))
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
     def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
@@ -26,15 +22,29 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/new_list/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+    def test_uses_home_template(self):
         response = self.client.get('/')
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+
+        response = self.client.get('/list/news_list/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text="Itemey 1")
+        Item.objects.create(text="Itemey 2")
+
+        response = self.client.get('/new_list/')
+
+        self.assertContains(response, 'Itemey 1')
+        self.assertContains(response, 'Itemey 2')
--- lists/views.py
+++ lists/views.py
@@ -6,6 +6,13 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST.get('item_text', ''))
-        return redirect('/')
+        return redirect('/lists/new_list/')
     items = Item.objects.all()
     return render(request, 'home.html', context={'items': items})
+
+
+def view_list(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST.get('item_text', ''))
+        return redirect('/lists/new_list/')
+    return render(request, 'list.html')
